e97e6ec905a647527903ee0bdd66249ffafe5dbc
Asygarr <105841104721@student.unismuh.ac.id>
2023-10-07 14:45:54 +0800

    initial commit frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, soluta.
+      </h2>
+    </div>
+  );
+};
+
+export default App;
