6d8251d721c15bfb1e82809ab600def40950239c
Asygarr <105841104721@student.unismuh.ac.id>
2023-10-09 15:30:21 +0800

    membuat component add product langsung terhubung ke backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable no-unused-vars */
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductList from "./components/ProductList";
+import AddProduct from "./components/AddProduct";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ProductList />} />
+        <Route path="/add" element={<AddProduct />} />
       </Routes>
     </BrowserRouter>
   );
